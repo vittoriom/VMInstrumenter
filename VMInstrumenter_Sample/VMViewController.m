978d42d8638d6bb4e46e9d8c0b16fcbc6bad8be7
Vittorio Monaco <vittorio.monaco1@gmail.com>
2013-11-02 12:50:13 +0100

    Updated comments as usage documentation

--- VMInstrumenter_Sample/VMViewController.m
+++ VMInstrumenter_Sample/VMViewController.m
@@ -18,50 +18,54 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 
+    //Get an instance of the Instrumenter
     VMDInstrumenter *instrumenter = [VMDInstrumenter sharedInstance];
     
+    //This will execute doFoo
     [self doFoo];
     
+    //From now on doFoo won't get executed anymore
     [instrumenter suppressSelector:@selector(doFoo) forInstancesOfClass:[self class]];
     
+    //Nothing happens if we suppress a selector twice, just a log in the console
     [instrumenter suppressSelector:@selector(doFoo) forInstancesOfClass:[self class]];
     
+    //This will do nothing
     [self doFoo];
     
+    //This will execute doBar
     [self doBar];
     
-    [instrumenter suppressSelector:@selector(doBar) forInstancesOfClass:[self class]];
-    
-    [self doBar];
-    
-    NSLog(@"Restoring selector doBar");
-    
-    [instrumenter restoreSelector:@selector(doBar) forInstancesOfClass:[self class]];
-    
-    [self doBar];
-    
+    //From now on every call to doBar will get a pre and post NSLog
     [instrumenter traceSelector:@selector(doBar) forClass:[self class]];
     
+    //Here you can see
     [self doBar];
     
+    //The same as before, but with a method that takes a parameter
     [instrumenter traceSelector:@selector(doCalculations:) forClass:[self class]];
     
+    //The tracing won't impact the normal return
     NSNumber *result = [self doCalculations:@2];
     
     NSLog(@"RESULT: %@",result);
     
+    //From now on every call to doFoo will execute doFoo again
     [instrumenter restoreSelector:@selector(doFoo) forInstancesOfClass:[self class]];
+    
+    //This swaps the implementations of doFoo and doBar
     [instrumenter replaceSelector:@selector(doFoo) ofClass:[self class] withSelector:@selector(doBar) ofClass:[self class]];
     [self doFoo];
     [self doBar];
     
+    //This swaps the implementations again (i.e. it restores them to their original state)
     [instrumenter replaceSelector:@selector(doFoo) ofClass:[self class] withSelector:@selector(doBar) ofClass:[self class]];
     [self doFoo];
     [self doBar];
     
-    [instrumenter traceSelector:@selector(doFoo) forClass:[self class] dumpingSelfObject:YES dumpingStackTrace:YES];
+    //This shows how to trace a selector dumping the stack trace
+    [instrumenter traceSelector:@selector(doFoo) forClass:[self class] dumpingStackTrace:YES];
     [self doFoo];
 }
 
