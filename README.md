9c1825befb08b57f4292f731d070b03c03cd2960
Vittorio Monaco <vittorio.monaco1@gmail.com>
2013-11-09 10:37:28 +0100

    No need for installing xctool on Travis

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
-[![Build Status](https://travis-ci.org/vittoriom/VMInstrumenter.png?branch=master)](https://travis-ci.org/vittoriom/VMInstrumenter)
-
 VMDInstrumenter
 ==============
 
--- Travis/script.sh
+++ Travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool -workspace VMInstrumenter_Sample.xcworkspace -scheme VMInstrumenter_Sample build test
+xctool -workspace VMInstrumenter_Sample.xcworkspace -scheme VMInstrumenter_Sample -sdk iphonesimulator6.1 clean build
